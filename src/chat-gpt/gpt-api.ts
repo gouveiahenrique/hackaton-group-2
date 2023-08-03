da11c1d21fd0ac2a1aca28667159a3b298f4e4ae
Danilo R Silva <danilo.rodriguesdasilva-ext@ab-inbev.com>
2023-08-03 15:29:00 -0300

    initial commit

--- src/chat-gpt/gpt-api.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
-
-const API_KEY = ''; // Replace this with your actual API key
-
-export const gptApi = new OpenAIClient(
-  'https://gptdeployment.openai.azure.com/',
-  new AzureKeyCredential(API_KEY)
-);
